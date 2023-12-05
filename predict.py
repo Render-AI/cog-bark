a8d31f3347ac2e9b5defa0c7e5bd3efd4724a127
PlatformKit <54647524+platform-kit@users.noreply.github.com>
2023-12-04 18:25:18 -0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -142,7 +142,7 @@ class Predictor(BasePredictor):
 
             print(type(audio_tokens))
             
-            audioPieces = np.hstack(audioPieces, audio_tokens)
+            audioPieces = audioPieces + audio_tokens
 
         from bark.generation import codec_decode
 
